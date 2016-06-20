415885e601e0743a2a5d28a97ad1714bb30a44ac
yuikanetaka <ykanetaka@idcf.jp>
2016-06-20 17:24:41 +0900

    :wink: 見た目を変更
    文字サイズを変更
    アイコンのファイル名を変更
    ポップアップの見た目を変更

--- popup/index.js
+++ popup/index.js
@@ -8,7 +8,7 @@ $(window).on("load", chrome.tabs.reload())
 $("#dailyreport").on("click", function () {
     var dt = new Date();
     var title = dt.getFullYear() + "/" + (dt.getMonth()+1) + "/" + dt.getDate() +" 日報"
-    var body = "<p><h1>◆テーマ</h1><h1>◆担当者名</h1></p><hr><p><h1>◆今日の目標</h1><h1>◆業務内容</h1>（時系列で）<h1>◆今日の成果</h1><h1>◆気づき・疑問点</h1><h1>◆課題</h1></p><hr><p><h1>◆明日の予定・目標</h1></p>"
+    var body = "<p><h3>◆テーマ</h3><h3>◆担当者名</h3></p><hr><p><h3>◆今日の目標</h3><h3>◆業務内容</h3>（時系列で）<h3>◆今日の成果</h3><h3>◆気づき・疑問点</h3><h3>◆課題</h3></p><hr><p><h3>◆明日の予定・目標</h3></p>"
     chrome.tabs.sendMessage(
         tabId,
         {
@@ -21,7 +21,7 @@ $("#dailyreport").on("click", function () {
 
 $("#weeklyreport").on("click", function () {
     var title = "【週報】"
-    var body = "<p><h1>◆今週の予定</h1><h1>◆今週の目標</h1></p><hr><p><h1>◆業務内容・結果</h1><h2>（月）</h2><h2>（火）</h2><h2>（水）</h2><h2>（木）</h2><h2>（金）</h2><h1>◆問題点・反省点</h1><h1>◆課題</h1></p><hr><p><h1>◆来週の予定・目標</h1></p>"
+    var body = "<p><h3>◆今週の予定</h3><h3>◆今週の目標</h3></p><hr><p><h3>◆業務内容・結果</h3><h4>（月）</h4><h4>（火）</h4><h4>（水）</h4><h4>（木）</h4><h4>（金）</h4><h3>◆問題点・反省点</h3><h3>◆課題</h3></p><hr><p><h3>◆来週の予定・目標</h3></p>"
     chrome.tabs.sendMessage(
         tabId,
         {
@@ -34,7 +34,7 @@ $("#weeklyreport").on("click", function () {
 
 $("#event").on("click", function () {
     var title = "【イベント】"
-    var body = "<p><h1>◆日時</h1><h1>◆場所</h1><h1>◆テーマ</h1><h1>◆講師</h1><h1>◆受講目的</h1></p><hr><p><h1>◆講義内容</h1><h1>◆所感</h1><h1>◆配布資料</h1><h1>◆備考</h1></p>"
+    var body = "<p><h3>◆日時</h3><h3>◆場所</h3><h3>◆テーマ</h3><h3>◆講師</h3><h3>◆受講目的</h3></p><hr><p><h3>◆講義内容</h3><h3>◆所感</h3><h3>◆配布資料</h3><h3>◆備考</h3></p>"
     chrome.tabs.sendMessage(
         tabId,
         {
